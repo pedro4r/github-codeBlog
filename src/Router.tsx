7e0260ff351753f99ad0d02608901b95dbe528d9
Pedro Requiao <alonsofts@gmail.com>
2023-09-23 23:13:04 -0400

    add: Workflow

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,7 +8,7 @@ export function Router() {
         <Routes>
             <Route path="/" element={<DefaultLayout />}>
                 <Route path="/github-codeBlog/" element={<Home />} />
-                <Route path="/github-codeBlog/post/:postId" element={<Post />} />
+                <Route path="post/:postId" element={<Post />} />
             </Route>
         </Routes>
     );
